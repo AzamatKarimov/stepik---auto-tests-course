934f851b182657918a525c03efd9f1fbf2916df0
Каримов Азамат Азатович <KarimovAA@moffice.akbars.ru>
2020-03-30 11:02:20 +0300

    2

--- test_items.py
+++ test_items.py
@@ -1,9 +1,17 @@
+import time
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 
 
-link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
-
-def test_guest_should_see_login_link(browser):
+def test_product_page_contains_a_button_to_add_to_cart(browser):
+    link = 'http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/'
+    browser.implicitly_wait(10)
     browser.get(link)
-    browser.find_element_by_css_selector("#login_link")
-    
-    
+    # time.sleep(30)
+
+    add_btn = WebDriverWait(browser, 10).until(
+        EC.visibility_of_element_located((By.CLASS_NAME, 'btn-add-to-basket'))
+    )
+
+    assert add_btn, 'success if add button exist'
